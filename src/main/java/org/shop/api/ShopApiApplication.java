eaca942da9ee8c97f138cc9198c516638b32aec3
zhengchao@cnlive.com <zhengchao@cnlive.com>
2019-08-02 11:43:44 +0800

    update

--- src/main/java/org/shop/api/ShopApiApplication.java
+++ src/main/java/org/shop/api/ShopApiApplication.java
@@ -5,19 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-/*@SpringBootApplication
-public class ShopApiApplication extends SpringBootServletInitializer {
-
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ShopApiApplication.class);
-    }
-
-
-    public static void main(String[] args) {
-        SpringApplication.run(ShopApiApplication.class, args);
-    }
-}*/
-
 
 @SpringBootApplication
 public class ShopApiApplication {
